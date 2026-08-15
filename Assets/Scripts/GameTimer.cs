2a5e27b1c976a2f38988ceb3a64bedf35fd01d52
Mykhailo <mihailvasiliev2309@gmail.com>
2026-08-15 17:11:17 +0300

    Add basic timer

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI; 
 
 public class GameTimer : MonoBehaviour
 {
     [SerializeField] private float timeRemaining = 120f; 
     [SerializeField] private bool timerIsRunning = true;
-
+    [SerializeField] private float fadeStartTime = 20f;
     [SerializeField] private TMP_Text timerText;
+    [SerializeField] private Image fadeImage; 
 
     public float TimeRemaining => timeRemaining; 
     public bool IsRunning => timerIsRunning;
@@ -19,18 +21,31 @@ public class GameTimer : MonoBehaviour
             {
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
+                UpdateFade(); 
             }
             else
             {
-                Debug.Log("Час вийшов!");
                 timeRemaining = 0;
                 timerIsRunning = false;
                 DisplayTime(timeRemaining);
+                UpdateFade(); 
             }
         }
     }
 
-    void DisplayTime(float timeToDisplay)
+    private void UpdateFade()
+    {
+        if (fadeImage != null && timeRemaining <= fadeStartTime)
+        {
+            float alpha = 1f - (timeRemaining / fadeStartTime);
+
+            Color currentColor = fadeImage.color;
+            currentColor.a = alpha;
+            fadeImage.color = currentColor;
+        }
+    }
+
+    private void DisplayTime(float timeToDisplay)
     {
         float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
